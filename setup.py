11eed9624d0de3a0bc864887cbf6378ac58493fd
tomerlange <langetomer@gmail.com>
2019-09-19 13:13:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='Analytics',
     version='1.0',
-    install_requires=['maxminddb-geolite2'],
+    install_requires=["pandas", "tensorflow", "maxminddb-geolite2"],
     py_modules=["utilities", "data_preparation", "machine_learning", "anomaly_processing"]
 )
